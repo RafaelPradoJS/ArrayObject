bc80ce936669917242131a32472a989cc084d46a
Rafael do Prado <rafa40026@gmail.com>
2025-04-14 21:27:37 -0300

    Add files via upload

---
+++ Array-Object/metodo-array/script.js
@@ -0,0 +1,29 @@
+// /*var frutas = ["Banana", "Laranja"]
+// /*push() Adiciona item no final da lista*/
+// frutas.push("Uva")
+// frutas.push("maça")
+
+// /* .unshift() Adiciona item no inicio da lista*/
+// frutas.unshift("manga")
+
+// /*.pop remove o ultimo item da lista*/
+// frutas.pop()
+
+// /*.shift remove o primeiro da lista*/
+// frutas.shift()
+
+// /* .splice posso remover qualquer um */
+// frutas.splice(1, 1)
+
+// var nomeFruta = prompt( "Digite a fruta que deseja deletar")
+
+// var indexFruta = frutas.indexoff (nomeFruta)
+
+// if(indexFruta != -1) {
+//     frutas.splice(indexFruta,1)
+// } else {
+//     alert("Fruta não existe")
+// }
+/* aparece se é false ou true*/
+console.log(frutas)
+console.log (frutas.includes("Kiwi"))
